cbb9c4483d170df361b6ddac6e9a4feb7c1aaaf9
Herminio García González <herminiogg@hotmail.com>
2024-01-24 12:09:24 +0100

    Fixed an error while converting to RML in code generation v2

--- ShExMLFrontEnd/codeGeneration/index.js
+++ ShExMLFrontEnd/codeGeneration/index.js
@@ -58,7 +58,7 @@ $("#submitButtonRML").click(function(){
     var content = {
         shexml: shexmldocument,
         format: $("#conversionFormat").val(),
-        prettify: false
+        prettify: "false"
     }
     $.ajax("https://shexml.herminiogarcia.com/api/generateRML", {
         "data": JSON.stringify(content),
